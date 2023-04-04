4da3fbdaa3d2be8c35f27fcc0c51615ad1345064
Thomas Hepner <hepner.thomas@gmail.com>
2023-04-04 16:30:33 -0700

    feat: replace Infura with Alchemy.

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,9 +1,12 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+// const INFURA_ID = ''
+// const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
-const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e'
+const ALCHEMY_API_KEY = '<INSERT ALCHEMY API  KEY HERE>'
+const provider = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`)
+
+const address = '0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe'
 
 const main = async () => {
     const balance = await provider.getBalance(address)
