6b8bd68c025fa72f3bc6c6a6f6948486693f9f51
Thomas Hepner <hepner.thomas@gmail.com>
2023-04-05 07:34:34 -0700

    feat: add dotenv library to retrieve environment variables from .env file and add .env file to .gitignore.

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,9 +1,7 @@
 const { ethers } = require("ethers");
+require('dotenv').config()
 
-// const INFURA_ID = ''
-// const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
-
-const ALCHEMY_API_KEY = '<INSERT ALCHEMY API  KEY HERE>'
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const provider = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`)
 
 const address = '0x1a171a91B4Aa1A669e2397D6670746DDcDd4fbBe'
